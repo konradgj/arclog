7a72e3fea9671a7df683869a07e9fc1fd2a30846
konradgj <konrad.gjerstad@gmail.com>
2025-09-24 22:35:56 +0200

    use user config dir for config

--- cmd/root.go
+++ cmd/root.go
@@ -4,19 +4,17 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/konradgj/arclog/cmd/config"
+	"github.com/konradgj/arclog/cmd/watch"
+	"github.com/konradgj/arclog/internal/appconfig"
 	"github.com/konradgj/arclog/internal/logger"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var (
 	verbose bool
-	cfgFile string
 
 	rootCmd = &cobra.Command{
 		Use:   "arclog",
@@ -39,52 +37,10 @@ func init() {
 	cobra.OnInitialize(func() {
 		logger.Init(verbose)
 	})
-	cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(appconfig.InitConfig)
 
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "Use config file (default is $HOME/.arclog.toml)")
 
 	rootCmd.AddCommand(config.ConfigCmd)
-}
-
-func initConfig() {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		logger.Error("Could not get home directory", "err", err)
-		os.Exit(1)
-	}
-
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		cobra.CheckErr(err)
-
-		viper.AddConfigPath(home)
-		viper.SetConfigType("toml")
-		viper.SetConfigName(".arclog")
-
-	}
-
-	viper.AutomaticEnv()
-
-	defaultLogPath := filepath.Join("Documents", "Guild Wars 2", "addons", "arcdps.cbtlogs")
-	viper.SetDefault("LogPath", filepath.Join(home, defaultLogPath))
-	viper.SetDefault("UserToken", "")
-
-	if err := viper.ReadInConfig(); err != nil {
-
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			if err = viper.SafeWriteConfig(); err != nil {
-				logger.Error("Could not create new config file", "err", err)
-				os.Exit(1)
-			}
-
-			viper.SetConfigFile(filepath.Join(home, ".arclog.toml"))
-			logger.Debug("Created new config file", "path", viper.ConfigFileUsed())
-		} else {
-			logger.Error("Could not read config", "err", err)
-			os.Exit(1)
-		}
-	}
-	fmt.Println("Using config file:", viper.ConfigFileUsed())
+	rootCmd.AddCommand(watch.WatchCmd)
 }
